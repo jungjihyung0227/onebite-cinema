22610df8d97d524dfc89b680dfe53c03c2510c17
정지형 <jh_jeong@dorosee.kr>
2025-04-02 22:44:36 +0900

    fix : 경로 변경

--- src/lib/fetch-random-movies.ts
+++ src/lib/fetch-random-movies.ts
@@ -1,7 +1,7 @@
 import { MovieData } from "@/types";
 
 export default async function fetchRandomMovies(): Promise<MovieData[]> {
-  const url = "https://onebite-cinema-api-main-tau.vercel.app//movie/random";
+  const url = "https://onebite-cinema-api-main-tau.vercel.app/movie/random";
 
   try {
     const response = await fetch(url);
