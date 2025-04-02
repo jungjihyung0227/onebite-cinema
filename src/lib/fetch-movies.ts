cc0546b8698117f8c0fe6571c5b70220424588fc
정지형 <jh_jeong@dorosee.kr>
2025-04-02 22:42:24 +0900

    fix : api 경로 수정

--- src/lib/fetch-movies.ts
+++ src/lib/fetch-movies.ts
@@ -1,7 +1,7 @@
 import { MovieData } from "@/types";
 
 export default async function fetchMovies(q?: string): Promise<MovieData[]> {
-  let url = "https://onebite-cinema-api-main-tau.vercel.app//movie";
+  let url = "https://onebite-cinema-api-main-tau.vercel.app/movie";
 
   if (q) {
     url += `/search?q=${q}`;
